62721f8239e9d8c04598cef1496e3c89a148f86c
nehahirve <neha.hirve@hyperisland.se>
2021-03-30 14:06:07 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const getRandomColor = () => {
+  const randomRGB = () => Math.floor(Math.random() * Math.floor(256))
+  return `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`
 }
 
-export default App;
+export default function App() {
+  const [color, setColor] = useState(getRandomColor())
+  const [buttonColor, setButtonColor] = useState(getRandomColor())
+  const [pos, setPos] = useState({ x: 0, y: 0 })
+
+  const updatePos = e => setPos({ x: e.clientX, y: e.clientY })
+
+  useEffect(() => {
+    window.addEventListener('mousemove', updatePos)
+    return () => window.removeEventListener('mousemove', updatePos)
+  }, [])
+
+  return (
+    <div
+      className='container'
+      style={{
+        backgroundColor: color
+      }}
+    >
+      <button
+        style={{
+          top: pos.y,
+          left: pos.x,
+          backgroundColor: buttonColor
+        }}
+        className='button'
+        onClick={() => {
+          setColor(getRandomColor)
+          setButtonColor(getRandomColor)
+        }}
+      ></button>
+    </div>
+  )
+}
